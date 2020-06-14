c28d12e1e8837c68ceadc3f3ea8d9b252ce0a1ef
Piyush Gupta <piyush16066@iiitd.ac.in>
2020-06-14 21:42:16 +0530

    Add generic params

--- jcoffee.py
+++ jcoffee.py
@@ -3,8 +3,10 @@ import argparse
 import subprocess
 
 RUNNER = "jcoffee_runner.py"
+OUT_PATH = "fixed/output.java"
+N_TRIES = 10
 
-def jcoffee(inp_file_path, out_file_path="output.java", times_to_try_compile=10):
+def jcoffee(inp_file_path, out_file_path=OUT_PATH, times_to_try_compile=N_TRIES):
 	subprocess.run(['./'+RUNNER, inp_file_path, out_file_path, str(times_to_try_compile)], stderr=subprocess.PIPE).stderr.decode('utf-8')
 
 
@@ -13,8 +15,8 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('-f', required=True, help="path to input java code snippet")
-	parser.add_argument('-o', default="output.java", help="path to output java file. default=output.java")
-	parser.add_argument('-n', type=int, default=10, help="max number of times to try compile. default=10")
+	parser.add_argument('-o', default=OUT_PATH, help="path to output java file. default="+OUT_PATH)
+	parser.add_argument('-n', type=int, default=N_TRIES, help="max number of times to try compile. default=10")
 	args = parser.parse_args()
 
 	INPUT_FILE = args.f
